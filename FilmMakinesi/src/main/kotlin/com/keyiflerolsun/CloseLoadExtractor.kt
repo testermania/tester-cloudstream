9562d7aa1dd004545c1cacb8ef63fa84297175bf
testermania <61498468+testermania@users.noreply.github.com>
2025-06-27 21:17:40 +0300

    54df202c4bad146298f251fce6ae20e894a03649 'nın Derlenmesi

---
+++ FilmMakinesi/src/main/kotlin/com/keyiflerolsun/CloseLoadExtractor.kt
@@ -0,0 +1,60 @@
+// ! Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
+
+package com.keyiflerolsun
+
+import android.util.Log
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.*
+import android.util.Base64
+
+private fun getm3uLink(data: String): String {
+    val first  = Base64.decode(data,Base64.DEFAULT).reversedArray()
+    val second = Base64.decode(first, Base64.DEFAULT)
+    val result = second.toString(Charsets.UTF_8).split("|")[1]
+
+    return result
+}
+
+open class CloseLoad : ExtractorApi() {
+    override val name            = "CloseLoad"
+    override val mainUrl         = "https://closeload.filmmakinesi.de"
+    override val requiresReferer = true
+
+    override suspend fun getUrl(url: String, referer: String?, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit) {
+        val extRef = referer ?: ""
+        Log.d("Kekik_${this.name}", "url » $url")
+
+        val iSource = app.get(url, referer = extRef)
+
+        iSource.document.select("track").forEach {
+            subtitleCallback.invoke(
+                SubtitleFile(
+                    lang = it.attr("label"),
+                    url  = fixUrl(it.attr("src"))
+                )
+            )
+        }
+
+        val obfuscatedScript = iSource.document.select("script[type=text/javascript]")[1].data().trim()
+        val rawScript        = getAndUnpack(obfuscatedScript)
+        val regex = Regex("var player=this\\}\\);var(.*?);myPlayer\\.src")
+        val matchResult = regex.find(rawScript)
+        if (matchResult != null) {
+            val extractedString = matchResult.groups[1]?.value?.trim()?.substringAfter("=\"")?.substringBefore("\"")
+            val m3uLink = Base64.decode(extractedString, Base64.DEFAULT).toString(Charsets.UTF_8)
+            Log.d("Kekik_${this.name}", "m3uLink » $m3uLink")
+
+            callback.invoke(
+            newExtractorLink(
+                source = this.name,
+                name = this.name,
+                url = m3uLink ?: throw ErrorLoadingException("m3u link not found"),
+                type = ExtractorLinkType.M3U8 // Tür olarak M3U8'yi belirtiyoruz
+            ) {
+                quality = Qualities.Unknown.value
+                headers = mapOf("Referer" to url)
+            }
+            )
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# ☁️ CloudStream için Türkçe Eklentiler
+
+## 💾 Kurulum
+
+1. **[cloudstream 4.5.2 indir /releases](https://github.com/recloudstream/cloudstream/releases/download/v4.5.2/4.5.2.apk)** _APK dosyasını indirip kurun._
+2. Eğer bazı eklentilerde sorun yaşıyorsanız pre-release indirin ve kurun. https://github.com/recloudstream/cloudstream/releases/download/pre-release/app-prerelease-release.apk
+3. **Uygulamayı açın `Ayarlar` - `Eklentiler` - `Depo Ekle` kısmına girin.**
+4. **Sağdaki Depo Url'lerinden birini kopyalayıp yapıştırın.** `https://t.ly/X8u16` veya `https://raw.githubusercontent.com/testermania/tester-cloudstream/master/repo.json`
+
+### 📺 » [Google TV Temel Kurulum Adımları](https://github.com/testermania/tester-cloudstream/blob/main/MiBox.md)
+
+## 📱 Repo İçeriği
+
+[![Repo](https://github.com/testermania/tester-cloudstream/raw/master/.github/Repo.jpg?raw=True)](https://raw.githubusercontent.com/testermania/tester-cloudstream/master/repo.json)
+
+## 🌐 Telif Hakkı ve Lisans
+
+* *Copyright (C) 2023 by* keyiflerolsun
+* [GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007]
